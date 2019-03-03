88e18a026b5e11c3c4a6e7df6263d5598a50e299
Ponsukeee <ponz1652@gmail.com>
2019-03-04 04:11:00 +0900

    initial commit

---
+++ IInputModule.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using VRUtils.Components;
+
+namespace VRUtils.InputModule
+{
+public interface IInputModule
+{
+    IInputModule HandleInput(InputType input, DeviceInfo deviceInfo);
+    void OnSet();
+    void OnUnset();
+}
+}
